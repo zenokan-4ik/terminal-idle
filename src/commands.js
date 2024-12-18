528fa51808c95075e3b2580d3b9c369ccae87e03
zenokan-4ik <zenokan4ik@gmail.com>
2024-12-18 19:36:58 +0300

    fixes (again)

--- src/commands.js
+++ src/commands.js
@@ -3,7 +3,7 @@ export const ProcessCommand = (command) => {
         case "clear":
             return ["clear", "$ "+command+"\n"]
         default:
-            if(command.toLowerCase().slice(0, 5)){
+            if(command.toLowerCase().slice(0, 5) == "create"){
                 let arg = command.toLowerCase().slice(7)
                 console.log(command.toLowerCase().slice(7, 0))
                 return ["create", `$ Created : ${arg}`]
